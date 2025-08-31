088ea285ca697301c976f3768706176c4c65433d
Rafael <alvarezrafaelat@gmail.com>
2025-08-31 15:33:10 -0400

    bugs arreglados

---
+++ .history/src/php/includes/header_20250830224904.php
@@ -0,0 +1,31 @@
+<header class="container-fluid">
+    <div class="row d-flex flex-row justify-content-center">
+        <div class="col-6 d-flex flex-row justify-content-start align-items-center ps-2">
+            <i class="bi bi-card-checklist ms-2 fs-1 color-white"></i>
+        </div>
+        <div class="col-6 d-flex justify-content-between align-items-center py-2">
+            <ul class="list-group list-group-horizontal w-100 m-0 p-0">
+                <li class="list-group-item bg-transparent border-0 flex-fill text-center">
+                    <a href="" class="text-decoration-none text-dark">
+                        <h4 class="text-white">Inicio</h4>
+                    </a>
+                </li>
+                <li class="list-group-item bg-transparent border-0 flex-fill text-center">
+                    <a href="" class="text-decoration-none text-dark">
+                        <h4 class="text-white">Acerca de</h4>
+                    </a>
+                </li>
+                <li class="list-group-item bg-transparent border-0 flex-fill text-center">
+                    <a href="" class="text-decoration-none text-dark">
+                        <h4 class="text-white">Servicios</h4>
+                    </a>
+                </li>
+                <li class="list-group-item bg-transparent border-0 flex-fill text-center">
+                    <a href="" class="btn border-white border-2">
+                        <h4 class="text-white mb-1">Iniciar Sesion</h4>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</header>
---
+++ .history/src/php/model/usuario_model_20250831145848.php
@@ -0,0 +1,72 @@
+<?php
+
+include_once(__DIR__ . "/../../config/APP.php");
+include_once(__DIR__ . "/../../config/SERVER.php");
+include_once(__DIR__ . "/main_model.php");
+
+class usuario_model extends main_model
+{
+
+    public static function iniciarSesion($correo, $contraseña)
+    {
+        $conexion = parent::conectarBD();
+
+        if (!$conexion) {
+            return ["ok" => false, "mensaje" => "Error al conectar a la base de datos"];
+        }
+
+        $stmt = $conexion->prepare("SELECT * FROM usuarios WHERE correo = ?");
+        $stmt->execute([$correo]);
+        $resultado = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if (!$resultado) {
+            return ["ok" => false, "mensaje" => "Usuario no registrado para $correo"];
+        }
+
+        if ($correo != $resultado["correo"] || (parent::verificarContraseña($contraseña, $resultado["contraseña"]) == false)) {
+            return ["ok" => false, "mensaje" => "Correo o contraseña incorrectos"];
+        }
+
+        return ["ok" => true, "mensaje" => "exito al iniciar sesion"];
+    }
+
+    public static function registrar($nombre, $apellido, $correo, $contraseña)
+    {
+        $conexion = parent::conectarBD();
+
+        if (!$conexion) {
+            return ["ok" => false, "mensaje" => "Error al conectar a la base de datos"];
+        }
+
+        if (self::correoValido($correo) == false) {
+            return ["ok" => false, "mensaje" => "El correo ya está registrado"];
+        }
+
+        $contraseñaHash = parent::hashearContraseña($contraseña);
+
+        $stmt = $conexion->prepare("INSERT INTO usuarios (nombre, apellido, correo, contraseña) VALUES (?, ?, ?, ?)");
+        $stmt->execute([$nombre, $apellido, $correo, $contraseñaHash]);
+
+        return ["ok" => true, "mensaje" => "Registro exitoso"];
+
+    }
+
+    public static function correoValido($correo)
+    {
+        $conexion = parent::conectarBD();
+
+        if (!$conexion) {
+            return ["ok" => false, "mensaje" => "Error al conectar a la base de datos"];
+        }
+
+        $stmt = $conexion->prepare("SELECT * FROM usuarios WHERE correo = ?");
+        $stmt->execute([$correo]);
+        $resultado = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if ($resultado["correo"] === $correo) {
+            return ["ok" => false, "mensaje" => "El correo ya está asociado a una cuenta"];
+        }
+
+        return ["ok" => false, "mensaje" => "Correo disponible"];;
+    }
+}
